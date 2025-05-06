36b89b72708d9dcd57ebb5572d35ff5758857c9c
Matthew <s4088802@student.rmit.edu.au>
2025-05-06 16:35:42 +1000

    all keys have been added to files

---
+++ data/InventoryC/inventory_c_keys.py
@@ -0,0 +1,8 @@
+from utils.rsa_utils import public_key_generation, private_key_generation
+
+p = 1014247300991039444864201518275018240361205111
+q = 904030450302158058469475048755214591704639633
+e = 1158749422015035388438057
+
+public_key_C = public_key_generation(p, q, e)
+private_key_C = private_key_generation(p , q, public_key_C)
